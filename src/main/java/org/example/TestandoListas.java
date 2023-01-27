6cbc6d72486fdaacc8b6542c42112f8bc8a5aaa7
JefersonT <jefersontorres.f@gmail.com>
2023-01-27 15:03:25 -0300

    AULA 01. Trabalhando com ArrayList

--- src/main/java/org/example/TestandoListas.java
+++ src/main/java/org/example/TestandoListas.java
@@ -1,7 +1,34 @@
 package org.example;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestandoListas {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String curso01 = "Java e Java.io";
+        String curso02 = "Java e Java.util";
+        String curso03 = "Java Collections";
+
+        ArrayList<String> cursos = new ArrayList<>();
+        cursos.add(curso01);
+        cursos.add(curso02);
+        cursos.add(curso03);
+
+        System.out.println(cursos);
+
+        cursos.remove(1);
+
+        System.out.println(cursos);
+
+        System.out.println("Primeiro Curso: " + cursos.get(0));
+
+        for (int i = 0; i < cursos.size(); i++) {
+            System.out.println("Aula : " + cursos.get(i));
+        }
+
+        System.out.println(cursos);
+        Collections.sort(cursos);
+        System.out.println(cursos);
+
     }
 }
