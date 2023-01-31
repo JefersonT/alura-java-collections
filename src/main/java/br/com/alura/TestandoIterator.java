b8a6fa85dad0deb4b032d94c8c99a22b22ffc6cd
JefersonT <jefersontorres.f@gmail.com>
2023-01-31 18:16:26 -0300

    AULA 08. Outros sets e iterators

---
+++ src/main/java/br/com/alura/TestandoIterator.java
@@ -0,0 +1,18 @@
+package br.com.alura;
+
+import java.util.*;
+
+public class TestandoIterator {
+    public static void main(String[] args) {
+
+        List<String> letras = new LinkedList<>();
+        letras.add("A");
+        letras.add("B");
+        letras.add("C");
+
+        Iterator<String> iterator = letras.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+}
